ba403433c13eef2b71822ed14cfe8a3d0eff9199
Nadia <nadiawellela@gmail.com>
2023-08-08 21:32:52 -0700

    __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Model that creates a unique file storage for the application"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
